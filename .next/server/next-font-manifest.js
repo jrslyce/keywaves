83b16ef044ed2373d6936b1d07b2ad56e73e0ade
Jared Steffes <fakebusy@MacBook-Pro-2.local>
2024-09-06 17:35:09 -0500

    damn imports are bonkers

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/fakebusy/Library/CloudStorage/OneDrive-Personal/aDevProjects/keywaves/keywaves/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "./auth-options"; // Assuming you have your auth options in a separate file
+import { authOptions } from "@/app/api/auth-options"; // Adjusted import path
 
 export const GET = NextAuth(authOptions);
 export const POST = NextAuth(authOptions);
