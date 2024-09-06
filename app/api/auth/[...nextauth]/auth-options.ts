b643a7f8093073c19e52a2fe1c039628296230f1
Jared Steffes <fakebusy@MacBook-Pro-2.local>
2024-09-06 17:24:27 -0500

    twitch auth wasn't working, retry

--- app/api/auth/[...nextauth]/auth-options.ts
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -1,12 +1,22 @@
-import type { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
+import TwitchProvider from "next-auth/providers/twitch";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/app/lib/prisma";
-// Import your providers here
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    // Configure your authentication providers here
+    TwitchProvider({
+      clientId: process.env.TWITCH_CLIENT_ID as string,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
+    }),
   ],
-  // Add any other NextAuth configuration options
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
 };
